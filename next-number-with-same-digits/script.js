fb3fa593ea5cc38bb946d093e831574aa9ef2af5
Claas Paaschburg <paaschburgc@gmail.com>
2024-07-19 18:12:41 +0200

    fix: ensure the code works with bigger numbers

--- next-number-with-same-digits/script.js
+++ next-number-with-same-digits/script.js
@@ -1,29 +1,65 @@
 function nextBigger(n) {
   let result = -1;
-  const number = n.toString();
-  const newNumber = n;
-  let Arr1 = [];
+  let firstSmaller = result;
+  const string = n.toString();
 
-  for (let i1 = 0; i1 < number.length; i1++) {
-    Arr1.push(number[i1]);
-  }
-
-  while (n.toString().length === number.length) {
-    let Arr2 = [];
-    n++;
-
-    for (let i2 = 0; i2 < n.toString().length; i2++) {
-      Arr2.push(n.toString()[i2]);
-      console.log("Arr1 " + Arr1.sort());
-      console.log("Arr2 " + Arr2.sort());
-
-      if (Arr1.sort() === Arr2.sort()) {
-        console.log("Treffer");
-        result = n;
-        break;
-      }
+  for (let i = string.length - 2; i >= 0; i--) {
+    if (string[i] < string[i + 1]) {
+      firstSmaller = i;
+      break;
     }
   }
 
+  if (firstSmaller === -1) {
+    return result;
+  }
+
+  let nextSmaller = firstSmaller + 1;
+  for (let i2 = nextSmaller; i2 < string.length; i2++) {
+    if (
+      string[i2] > string[firstSmaller] &&
+      string[i2] <= string[nextSmaller]
+    ) {
+      nextSmaller = i2;
+    }
+  }
+
+  const digits = string.split("");
+  [digits[firstSmaller], digits[nextSmaller]] = [
+    digits[nextSmaller],
+    digits[firstSmaller],
+  ];
+
+  const toRearrange = digits.splice(firstSmaller + 1).sort();
+  result = Number(digits.concat(toRearrange).join(""));
+
   return result;
 }
+
+// works with small numbers but very inefficient with larger numbers
+// function nextBigger(n) {
+//   let result = -1;
+//   const number = n.toString();
+//   const newNumber = n;
+//   let Arr1 = [];
+
+//   for (let i1 = 0; i1 < number.length; i1++) {
+//     Arr1.push(number[i1]);
+//   }
+
+//   while (n.toString().length === number.length) {
+//     let Arr2 = [];
+//     n++;
+
+//     for (let i2 = 0; i2 < n.toString().length; i2++) {
+//       Arr2.push(n.toString()[i2]);
+
+//       if(Arr1.length === Arr2.length) {
+//       if (Arr1.sort().toString() === Arr2.sort().toString()) {
+
+//         return n
+//       }}
+//     }
+//   }
+
+// }
